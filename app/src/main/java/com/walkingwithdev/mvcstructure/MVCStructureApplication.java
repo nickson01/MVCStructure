744d3764dea497690daa45ce8deca1fd14b87cc8
Wittavat Kanyarong <nickson_kv@hotmail.com>
2016-05-22 15:58:46 +0700

    init commit

---
+++ app/src/main/java/com/walkingwithdev/mvcstructure/MVCStructureApplication.java
@@ -0,0 +1,18 @@
+package com.walkingwithdev.mvcstructure;
+
+import android.app.Application;
+
+/**
+ * Created by Nick on 5/22/2016 AD.
+ */
+public class MVCStructureApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
